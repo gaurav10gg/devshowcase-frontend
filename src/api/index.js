405d1cd4627bdf5c8f2ca3c42d5dacd5098ce003
Gaurav <gauravgjee2025@gmail.com>
2025-12-10 12:23:49 +0530

    fixed api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:5000/api"; // change if needed
+export const API_URL = "https://devshowcase-backen.onrender.com/api"; // change if needed
 
 // token stored in localStorage after Supabase login
 export function getAuthHeaders() {
